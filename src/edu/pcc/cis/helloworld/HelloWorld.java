fe89590b667755d6ba427daff1b8c1ca5cb2980a
Kanako Matsumoto <kanako.matsumoto1@pcc.edu>
2023-10-23 20:41:32 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,7 +4,10 @@ package edu.pcc.cis.helloworld;
  * Class
  *
  * @author Kanako Matsumoto
- * @version 2023.
+ * @version 2023.10.23
  **/
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
